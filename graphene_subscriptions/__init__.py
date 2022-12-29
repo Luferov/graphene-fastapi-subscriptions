ae3b878d6bef4599b496adf1d387eca7235a0447
Victor <lyferov@yandex.ru>
2022-12-29 15:31:44 +0300

    fix: Poetry setting version

--- graphene_subscriptions/__init__.py
+++ graphene_subscriptions/__init__.py
@@ -2,3 +2,5 @@
 from .decorators import subscription_bind
 from .layers import BaseChannelLayer, InMemoryChannelLayer, RedisPubSubChannelLayer
 from .subscription import Subscription
+
+__version__: str = '0.1.0'
